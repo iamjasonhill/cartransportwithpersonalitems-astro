57b290b60e0453c809113555ce77811feb2956f0
Jason Hill <jason@jasonhill.com.au>
2026-03-12 14:49:19 +1000

    Bootstrap Astro starter for cartransportwithpersonalitems.com.au

---
+++ src/pages/llms.txt.ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from 'astro';
+import { site } from '../lib/site';
+
+export const prerender = true;
+
+export const GET: APIRoute = () => {
+  return new Response(
+    [
+      '# llms.txt',
+      `Site: ${site.name}`,
+      `Canonical: ${site.url}`,
+      `Primary sitemap: ${site.url.replace(/\/$/, '')}/sitemap-index.xml`,
+      '',
+      `- ${site.url.replace(/\/$/, '')}/sitemap-index.xml`,
+      `- ${site.url.replace(/\/$/, '')}/sitemap.html`,
+      `- ${site.url.replace(/\/$/, '')}/about/`,
+      `- ${site.url.replace(/\/$/, '')}/contact/`,
+      `- ${site.url.replace(/\/$/, '')}/privacy/`,
+      `- ${site.url.replace(/\/$/, '')}/terms/`,
+    ].join('\n'),
+    {
+      headers: {
+        'content-type': 'text/plain; charset=utf-8',
+      },
+    },
+  );
+};
